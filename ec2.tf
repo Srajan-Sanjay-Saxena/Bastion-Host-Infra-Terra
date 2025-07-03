01469785e0f8d867e2668c7eb5c16a37c8186b39
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2025-07-04 02:00:25 +0530

    Added private ec2

--- ec2.tf
+++ ec2.tf
@@ -10,6 +10,20 @@ tags = {
   Environment = "dev"
 }
 }
+resource "aws_instance" "private_mumbai_ec2" {
+    for_each = toset(data.aws_subnets.mumbai_private_subnet_ids.ids)
+  key_name = aws_key_pair.ssh-key.key_name
+ami = "ami-0f918f7e67a3323f0"
+instance_type = "t2.micro"
+subnet_id = each.value
+vpc_security_group_ids = [aws_security_group.only_ssh.id]
+tags = {
+  Name = "private_ec2_mumbai"
+  Environment = "dev"
+}
+}
+
+
 resource "aws_key_pair" "ssh-key" {
   key_name = "terraform-testing"
   public_key = file("${path.module}/id_terraform.pub")
